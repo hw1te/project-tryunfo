b9bf64b5181b141d691ae69378fae67b1dbaa224
hw1te <gutxander@gmail.com>
2022-02-26 21:13:05 -0300

    Requisito 1 feito.

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,28 +2,46 @@ import React, { Component } from 'react';
 
 class Form extends Component {
   render() {
-
     return (
-    <div>
-      <label>name</label>
-      <input type="text" data-testid="name-input" name="name"></input>
-      <label>Descrição</label>
-      <textarea datat-testid="description-input"></textarea>
-      <label>Attr 1</label>
-      <input type="number" data-testid="attr1-input"></input>
-      <label>Attr 2</label>
-      <input type="number" data-testid="attr2-input"></input>
-      <label>Attr 3</label>
-      <input type="number" data-testid="attr3-input"></input>
-      <label>Imagem</label>
-      <input type="text" data-testid="image-input"></input>
-      <label>Raridade</label>
-      <select placeholder='Selecione a raridade da carta'>
-        <opt>normal</opt>
-        <opt>raro</opt>
-        <opt>muito</opt>
-      </select>
-    </div>
+      <form>
+        <label htmlFor="name-input">
+          Nome
+          <input type="text" data-testid="name-input" />
+        </label>
+        <label htmlFor="description-input">
+          Descrição
+          <textarea data-testid="description-input" />
+        </label>
+        <label htmlFor="attr1-input">
+          Attr01
+          <input type="number" data-testid="attr1-input" />
+        </label>
+        <label htmlFor="attr2-input">
+          Attr02
+          <input type="number" data-testid="attr2-input" />
+        </label>
+        <label htmlFor="attr3-input">
+          Attr03
+          <input type="number" data-testid="attr3-input" />
+        </label>
+        <label htmlFor="image-input">
+          Imagem
+          <input type="text" data-testid="image-input" />
+        </label>
+        <label htmlFor="rare-input">
+          Raridade
+          <select data-testid="rare-input">
+            <option>normal</option>
+            <option>raro</option>
+            <option>muito raro</option>
+          </select>
+        </label>
+        <label htmlFor="trunfo-input">
+          <input type="checkbox" data-testid="trunfo-input" />
+          Super Trybe Trunfo
+        </label>
+        <button type="submit"data-testid="save-button">Salvar</button>
+      </form>
     );
   }
 }
